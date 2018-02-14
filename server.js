1ee3bf2c565b1f9309e28c304e588ead3de29326
Ghislain Antony Vaillant <ghislain@visulytix.com>
2018-02-14 20:43:24 +0000

    Blame the milk

---
+++ server.js
@@ -0,0 +1,25 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+
+var app = express();
+app.use(bodyParser.text());
+
+function convertToCaps(str) {
+    return str.toUpperCase();
+}
+
+app.get('/', function(req,res) {
+    res.end('Milk was a bad choice');
+});
+
+app.post('/api/capitalize', function(req, res) {
+    res.send(convertToCaps(req.body));
+})
+
+if (require.main === module) {
+    app.listen(port, function() {
+        console.log('Server listening on port: ' + port);
+    });
+}
